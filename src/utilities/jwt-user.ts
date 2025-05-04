7b27daf739bec4822f120c548fd6d974be9b1fa3
iamswart <swartjide@gmail.com>
2025-05-04 23:14:22 +0100

    initital implementation

--- src/utilities/jwt-user.ts
+++ src/utilities/jwt-user.ts
@@ -1,17 +1,12 @@
 export class JWTUser {
   readonly id: string;
-  readonly isAdmin: boolean;
 
   constructor(details: any) {
     this.id = details.id;
-    this.isAdmin = details.isAdmin;
   }
 
   getId(): string {
     return this.id;
   }
 
-  getIsAdmin(): boolean {
-    return this.isAdmin;
-  }
 }
